1fcc237e2139796772036f58d05b20495bbf2d67
Matsvei Haidukou <s26169@pjwstk.edu.pl>
2026-03-30 15:37:22 +0300

    Add CalculateMin skeleton

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -18,5 +18,25 @@ public static class StatisticsHelper
 
         return (double)sum / values.Length;
     }
+
+    public static int CalculateMax(int[] values)
+{
+    if (values.Length == 0)
+        return 0;
+
+    int max = values[0];
+    foreach (int value in values)
+    {
+        if (value > max)
+            max = value;
+    }
+
+    return max;
+}
+
+public static int CalculateMin(int[] values)
+{
+    return 0;
+}
 }
 
