f2efcf72f7a8e6eb95ae578b4006a94ffb5c3276
Matsvei Haidukou <s26169@pjwstk.edu.pl>
2026-03-30 15:37:33 +0300

    Implement CalculateMin logic

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -36,7 +36,17 @@ public static class StatisticsHelper
 
 public static int CalculateMin(int[] values)
 {
-    return 0;
+    if (values.Length == 0)
+        return 0;
+
+    int min = values[0];
+    foreach (int value in values)
+    {
+        if (value < min)
+            min = value;
+    }
+
+    return min;
 }
 }
 
