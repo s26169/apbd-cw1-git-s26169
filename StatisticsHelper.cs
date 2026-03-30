b4cb17f6b87d754e1367daa4b13988982a83933b
Matsvei Haidukou <s26169@pjwstk.edu.pl>
2026-03-30 15:19:15 +0300

    Add CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -4,4 +4,19 @@ public static class StatisticsHelper
     {
         return number * number;
     }
+
+    public static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0)
+            return 0;
+
+        int sum = 0;
+        foreach (var v in values)
+        {
+            sum += v;
+        }
+
+        return (double)sum / values.Length;
+    }
 }
+
